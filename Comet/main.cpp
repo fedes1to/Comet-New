ebb016d9de659341135fcf1b0d6a32f6b0a980bf
7ddf-1 <l7n7002@gmail.com>
2024-10-25 00:34:59 +0400

    Update main.cpp

--- Comet/main.cpp
+++ Comet/main.cpp
@@ -1,151 +1,187 @@
-#define CURL_STATICLIB
+#pragma once
+std::string version, objectName;
 
-#pragma comment (lib,"external/libcurl/lib/libcurl_a.lib")
-#pragma comment (lib,"Normaliz.lib")
-#pragma comment (lib,"Ws2_32.lib")
-#pragma comment (lib,"Wldap32.lib")
-#pragma comment (lib,"Crypt32.lib")
+typedef void (*CommandFunction)();
 
-#include "includes.h"
-extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+HINSTANCE hDll;
 
-Present oPresent;
-HWND window = NULL;
-WNDPROC oWndProc;
-ID3D11Device* pDevice = NULL;
-ID3D11DeviceContext* pContext = NULL;
-ID3D11RenderTargetView* mainRenderTargetView;
-WNDPROC originalWindowProcess = nullptr;
-HWND hwnd;
-HANDLE pHandle;
-
-static float imguifontsize = 23.0f;
-bool hasSetWindowSize = false;
-
-void createConsole() {
-	AllocConsole();
-	FILE* f;
-	freopen_s(&f, "CONOUT$", "w", stdout);
+void LoadFunctions() {
+	hDll = LoadLibrary("IpConfigCommands.dll");
 }
 
-void InitImGui() {
-	ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO();
-	io.ConfigFlags = ImGuiConfigFlags_NoMouseCursorChange;
-
-	ImFontConfig config;
-	config.FontDataOwnedByAtlas = false;
-	io.Fonts->AddFontFromFileTTF("C:\\Windows\\Fonts\\Verdana.ttf", imguifontsize, &config);
-
-	ImGui_ImplWin32_Init(window);
-	ImGui_ImplDX11_Init(pDevice, pContext);
-}
-
-LRESULT __stdcall WndProc(const HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-	if (true && ImGui_ImplWin32_WndProcHandler(hWnd, uMsg, wParam, lParam))
-		return true;
-
-	return CallWindowProc(oWndProc, hWnd, uMsg, wParam, lParam);
-}
-
-
-bool init = false;
-HRESULT __stdcall hkPresent(IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT Flags) {
-	if (!init) {
-		if (SUCCEEDED(pSwapChain->GetDevice(__uuidof(ID3D11Device), (void**)&pDevice))) {
-			pDevice->GetImmediateContext(&pContext);
-			DXGI_SWAP_CHAIN_DESC sd;
-			pSwapChain->GetDesc(&sd);
-			window = sd.OutputWindow;
-			ID3D11Texture2D* pBackBuffer;
-			pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
-			pDevice->CreateRenderTargetView(pBackBuffer, NULL, &mainRenderTargetView);
-			pBackBuffer->Release();
-			oWndProc = (WNDPROC)SetWindowLongPtr(window, GWLP_WNDPROC, (LONG_PTR)WndProc);
-			InitImGui();
-			init = true;
-		}
-		else {
-			return oPresent(pSwapChain, SyncInterval, Flags);
-		}
+void UnloadFunctions() {
+	if (hDll) {
+		FreeLibrary(hDll);
 	}
+}
 
-	ImGui_ImplDX11_NewFrame();
-	ImGui_ImplWin32_NewFrame();
-	ImGui::NewFrame();
-
-	if (!hasSetWindowSize) {
-		ImGui::SetNextWindowSize(ImVec2(900, 650));
-		hasSetWindowSize = true;
-	}
-	SetupImGuiStyle();
-	DrawESP();
-	if (menuOpen)
-		DrawMenu();
-	
-	ImGui::Render();
-	
-	pContext->OMSetRenderTargets(1, &mainRenderTargetView, NULL);
-	ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
-	return oPresent(pSwapChain, SyncInterval, Flags);
+CommandFunction GetFunction(const char* funcName) {
+	return (CommandFunction)GetProcAddress(hDll, funcName); 
 }
 
 
-DWORD WINAPI MainThread(LPVOID lpReserved) {
-	bool init_hook = false;
-	do {
-		if (kiero::init(kiero::RenderType::D3D11) == kiero::Status::Success) {
-			fslog::info("Initializing il2cpp");
-			IL2CPP::Initialize();
-			fslog::info("Il2cpp initialized");
-			createConsole();
-			fslog::info("*Credit [fedesito], [0SD47 - Useless]", 1); //if you delete 0SD47 you are gay
-			fslog::info("*Do not make a selection on this console!*\n\tPress the HOME button to close/open Comet!\n\n", 1);
 
-			fslog::info("Initializing pointers");
-			initPointers();
-			fslog::info("Pointers initialized");
 
-			kiero::bind(8, (void**)&oPresent, hkPresent);
-			fslog::info("Calling hooks");
-			hooks();
-			fslog::info("Hooks called");
-			init_hook = true;
-			hwnd = FindWindowA(0, ("PixelStrike3D"));
-			GetWindowThreadProcessId(hwnd, &pid);
-			pHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
 
-			fslog::info("[Info] Comet loaded, PID: {}", pid);
-		}
-	} while (!init_hook);
 
-	std::thread keyStateThread([]() {
-		while (true) {
-			if ((GetAsyncKeyState(VK_HOME) & 1)) {
-				menuOpen = !menuOpen;
+
+
+
+
+
+int buyAmount = 0;
+
+void SetupImGuiStyle()
+{
+	ImVec4* colors = ImGui::GetStyle().Colors;
+	colors[ImGuiCol_Text] = ImVec4(1.00f, 1.00f, 1.00f, 1.00f);
+	colors[ImGuiCol_TextDisabled] = ImVec4(0.50f, 0.50f, 0.50f, 1.00f);
+	colors[ImGuiCol_WindowBg] = ImVec4(0.10f, 0.10f, 0.10f, 1.00f);
+	colors[ImGuiCol_ChildBg] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+	colors[ImGuiCol_PopupBg] = ImVec4(0.19f, 0.19f, 0.19f, 0.92f);
+	colors[ImGuiCol_Border] = ImVec4(0.19f, 0.19f, 0.19f, 0.29f);
+	colors[ImGuiCol_BorderShadow] = ImVec4(0.00f, 0.00f, 0.00f, 0.24f);
+	colors[ImGuiCol_FrameBg] = ImVec4(0.05f, 0.05f, 0.05f, 0.54f);
+	colors[ImGuiCol_FrameBgHovered] = ImVec4(0.19f, 0.19f, 0.19f, 0.54f);
+	colors[ImGuiCol_FrameBgActive] = ImVec4(0.20f, 0.22f, 0.23f, 1.00f);
+	colors[ImGuiCol_TitleBg] = ImVec4(0.00f, 0.00f, 0.00f, 1.00f);
+	colors[ImGuiCol_TitleBgActive] = ImVec4(0.06f, 0.06f, 0.06f, 1.00f);
+	colors[ImGuiCol_TitleBgCollapsed] = ImVec4(0.00f, 0.00f, 0.00f, 1.00f);
+	colors[ImGuiCol_MenuBarBg] = ImVec4(0.14f, 0.14f, 0.14f, 1.00f);
+	colors[ImGuiCol_ScrollbarBg] = ImVec4(0.05f, 0.05f, 0.05f, 0.54f);
+	colors[ImGuiCol_ScrollbarGrab] = ImVec4(0.34f, 0.34f, 0.34f, 0.54f);
+	colors[ImGuiCol_ScrollbarGrabHovered] = ImVec4(0.40f, 0.40f, 0.40f, 0.54f);
+	colors[ImGuiCol_ScrollbarGrabActive] = ImVec4(0.56f, 0.56f, 0.56f, 0.54f);
+	colors[ImGuiCol_CheckMark] = ImVec4(0.33f, 0.67f, 0.86f, 1.00f);
+	colors[ImGuiCol_SliderGrab] = ImVec4(0.34f, 0.34f, 0.34f, 0.54f);
+	colors[ImGuiCol_SliderGrabActive] = ImVec4(0.56f, 0.56f, 0.56f, 0.54f);
+	colors[ImGuiCol_Button] = ImVec4(0.05f, 0.05f, 0.05f, 0.54f);
+	colors[ImGuiCol_ButtonHovered] = ImVec4(0.19f, 0.19f, 0.19f, 0.54f);
+	colors[ImGuiCol_ButtonActive] = ImVec4(0.20f, 0.22f, 0.23f, 1.00f);
+	colors[ImGuiCol_Header] = ImVec4(0.00f, 0.00f, 0.00f, 0.52f);
+	colors[ImGuiCol_HeaderHovered] = ImVec4(0.00f, 0.00f, 0.00f, 0.36f);
+	colors[ImGuiCol_HeaderActive] = ImVec4(0.20f, 0.22f, 0.23f, 0.33f);
+	colors[ImGuiCol_Separator] = ImVec4(0.28f, 0.28f, 0.28f, 0.29f);
+	colors[ImGuiCol_SeparatorHovered] = ImVec4(0.44f, 0.44f, 0.44f, 0.29f);
+	colors[ImGuiCol_SeparatorActive] = ImVec4(0.40f, 0.44f, 0.47f, 1.00f);
+	colors[ImGuiCol_ResizeGrip] = ImVec4(0.28f, 0.28f, 0.28f, 0.29f);
+	colors[ImGuiCol_ResizeGripHovered] = ImVec4(0.44f, 0.44f, 0.44f, 0.29f);
+	colors[ImGuiCol_ResizeGripActive] = ImVec4(0.40f, 0.44f, 0.47f, 1.00f);
+	colors[ImGuiCol_Tab] = ImVec4(0.00f, 0.00f, 0.00f, 0.52f);
+	colors[ImGuiCol_TabHovered] = ImVec4(0.14f, 0.14f, 0.14f, 1.00f);
+	colors[ImGuiCol_TabActive] = ImVec4(0.20f, 0.20f, 0.20f, 0.36f);
+	colors[ImGuiCol_TabUnfocused] = ImVec4(0.00f, 0.00f, 0.00f, 0.52f);
+	colors[ImGuiCol_TabUnfocusedActive] = ImVec4(0.14f, 0.14f, 0.14f, 1.00f);
+	colors[ImGuiCol_PlotLines] = ImVec4(1.00f, 0.00f, 0.00f, 1.00f);
+	colors[ImGuiCol_PlotLinesHovered] = ImVec4(1.00f, 0.00f, 0.00f, 1.00f);
+	colors[ImGuiCol_PlotHistogram] = ImVec4(1.00f, 0.00f, 0.00f, 1.00f);
+	colors[ImGuiCol_PlotHistogramHovered] = ImVec4(1.00f, 0.00f, 0.00f, 1.00f);
+	colors[ImGuiCol_TableHeaderBg] = ImVec4(0.00f, 0.00f, 0.00f, 0.52f);
+	colors[ImGuiCol_TableBorderStrong] = ImVec4(0.00f, 0.00f, 0.00f, 0.52f);
+	colors[ImGuiCol_TableBorderLight] = ImVec4(0.28f, 0.28f, 0.28f, 0.29f);
+	colors[ImGuiCol_TableRowBg] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+	colors[ImGuiCol_TableRowBgAlt] = ImVec4(1.00f, 1.00f, 1.00f, 0.06f);
+	colors[ImGuiCol_TextSelectedBg] = ImVec4(0.20f, 0.22f, 0.23f, 1.00f);
+	colors[ImGuiCol_DragDropTarget] = ImVec4(0.33f, 0.67f, 0.86f, 1.00f);
+	colors[ImGuiCol_NavHighlight] = ImVec4(1.00f, 0.00f, 0.00f, 1.00f);
+	colors[ImGuiCol_NavWindowingHighlight] = ImVec4(1.00f, 0.00f, 0.00f, 0.70f);
+	colors[ImGuiCol_NavWindowingDimBg] = ImVec4(1.00f, 0.00f, 0.00f, 0.20f);
+	colors[ImGuiCol_ModalWindowDimBg] = ImVec4(1.00f, 0.00f, 0.00f, 0.35f);
+
+	ImGuiStyle& style = ImGui::GetStyle();
+	style.WindowPadding = ImVec2(8.00f, 8.00f);
+	style.FramePadding = ImVec2(5.00f, 2.00f);
+	style.CellPadding = ImVec2(6.00f, 6.00f);
+	style.ItemSpacing = ImVec2(6.00f, 6.00f);
+	style.ItemInnerSpacing = ImVec2(6.00f, 6.00f);
+	style.TouchExtraPadding = ImVec2(0.00f, 0.00f);
+	style.IndentSpacing = 25;
+	style.ScrollbarSize = 15;
+	style.GrabMinSize = 10;
+	style.WindowBorderSize = 1;
+	style.ChildBorderSize = 1;
+	style.PopupBorderSize = 1;
+	style.FrameBorderSize = 1;
+	style.TabBorderSize = 1;
+	style.WindowRounding = 7;
+	style.ChildRounding = 4;
+	style.FrameRounding = 3;
+	style.PopupRounding = 4;
+	style.ScrollbarRounding = 9;
+	style.GrabRounding = 3;
+	style.LogSliderDeadzone = 4;
+	style.TabRounding = 4;
+}
+
+int currentTab = 100;
+
+void DrawMenu() {
+	ImVec2 displaySize = ImGui::GetIO().DisplaySize;
+	ImVec2 windowSize = ImVec2(displaySize.x * 0.3f, displaySize.y * 0.57f);
+	ImGui::SetNextWindowSize(windowSize);
+	ImGuiWindowFlags windowflags = ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoScrollbar |
+		ImGuiWindowFlags_NoScrollWithMouse | ImGuiWindowFlags_NoResize;
+	ImGui::Begin("Comet", &menuOpen, windowflags);
+	{
+		ImGui::BeginChild("TButtons", ImVec2(135, 145), true);
+		if (ImGui::Button("Gameplay")) currentTab = 0;
+		if (ImGui::Button("Visual")) currentTab = 1;
+		if (ImGui::Button("Account")) currentTab = 2;
+		if (ImGui::Button("Info")) currentTab = 3;
+		ImGui::EndChild();
+
+		ImGui::SameLine();
+		ImGui::BeginChild("Content", ImVec2(0, 0), true);
+
+		switch (currentTab) {
+		case 0:
+			ImGui::Checkbox("Triggerbot", (bool*)&triggerbot);
+
+			ImGui::Checkbox("Fast Bullet", (bool*)&test17);
+			ImGui::Checkbox("Rapid Fire", (bool*)&firerate);
+			ImGui::SameLine();
+			ImGui::Text("[Risk it?]");
+			ImGui::Checkbox("Grenade Bullet", (bool*)&GrenadeBullet);
+			ImGui::SameLine();
+			ImGui::Text("[Risk it?]");
+			ImGui::Checkbox("Insta Scope", (bool*)&InstaScope);
+			ImGui::Checkbox("Long Melee", (bool*)&LongMelee);
+			ImGui::Checkbox("Rapid Equipment", (bool*)&RapidEquipment);
+			ImGui::SameLine();
+			ImGui::Text("[Risk it?]");
+			ImGui::Checkbox("Unlimited Ammo", (bool*)&ammo);
+			ImGui::Checkbox("No Recoil", (bool*)&recoil);
+			ImGui::Checkbox("Infinite Equipment", (bool*)&eqcooldown);
+			ImGui::Checkbox("Fake Head Pos", (bool*)&FakeHeadPos);
+			ImGui::Checkbox("Fly Hack", (bool*)&FlyHack);
+			ImGui::Checkbox("Bonus Speed Hack", (bool*)&bonusSpeedHack);
+			ImGui::SameLine();
+			ImGui::Text("[Risk it?]");
+			ImGui::Checkbox("Always get free cases at end of match", (bool*)&getfreecases);
+			break;
+
+		case 1:
+			ImGui::Checkbox("Box", (bool*)&esp);
+			if (ImGui::CollapsingHeader("ESP - Properties")) {
+				ImGui::Checkbox("ESP: Show teammates", (bool*)&esp_teammates);
 			}
-			std::this_thread::sleep_for(std::chrono::milliseconds(50));
+			break;
+
+		case 2:
+			if (ImGui::Button("Get VIP")) buyVIP();
+			if (ImGui::Button("Get MVP")) buyMVP();
+			ImGui::Text("+++ Coming soon");
+			break;
+
+		case 3:
+			ImGui::TextUnformatted("Version:");
+			if (version.empty())
+				version = Application$$Version()->ToString();
+			ImGui::Text(version.c_str());
+			break;
 		}
-	});
 
-	keyStateThread.join();
-
-	return TRUE;
-}
-
-//Freeze
-//Freeze
-//Freeze
-//UnFreeze
-BOOL WINAPI DllMain(HMODULE hMod, DWORD dwReason, LPVOID lpReserved) {
-	switch (dwReason) {
-	case DLL_PROCESS_ATTACH:
-		DisableThreadLibraryCalls(hMod);
-		CreateThread(nullptr, 0, MainThread, hMod, 0, nullptr);
-		break;
-	case DLL_PROCESS_DETACH:
-		kiero::shutdown();
-		break;
+		ImGui::EndChild();
 	}
-	return TRUE;
+	ImGui::End();
 }
